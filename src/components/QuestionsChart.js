ca2a96c572c802151859f53c9750db6fd5ad1f5f
coderhook <abel1987@hotmail.es>
2019-07-23 11:11:55 +0200

    standBy all the set intervals

--- src/components/QuestionsChart.js
+++ src/components/QuestionsChart.js
@@ -55,7 +55,8 @@ export default class QuestionsChart extends Component {
   componentDidMount() {
     this.getQuestionsData()
     //makes another request to the server every 10 seconds
-    setInterval(this.getQuestionsData, 10000)
+
+    // setInterval(this.getQuestionsData, 10000)
   }
 
   render() {
