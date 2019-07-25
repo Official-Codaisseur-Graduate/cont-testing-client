08324656b1f3e9884895a76d9f4695d7b63fd997
coderhook <abel1987@hotmail.es>
2019-07-25 14:50:16 +0200

    add html/css for select version

--- src/components/SelectDate/SelectDateContainer.js
+++ src/components/SelectDate/SelectDateContainer.js
@@ -13,6 +13,7 @@ class SelectDateContainer extends Component {
     })
 
     this.props.setDate(event.target.value)
+
     this.props.getQuestionsData(event.target.value);
     this.props.getQuestionsStudentsData(event.target.value)
     this.props.getStudentsData(event.target.value)
@@ -27,13 +28,17 @@ class SelectDateContainer extends Component {
 
     return (
       <div>
-        <select className="select-css" name="" id="selectDate" onChange={this.handleChange}>
+        <select className="select-css" id="selectDate" onChange={this.handleChange}>
           <option>Select range of Data</option>
           <option value="today">Today</option>
           <option value="lastWeek">Last week</option>
           <option value="lastMonth">Last Month</option>
           <option value="lastYear">Last Year</option>
           <option value="allData">All Data</option>
+        </select>
+        
+        <select className="select-css sel-version" id="">
+          <option>Select Version</option>
         </select>
       </div>
     )
@@ -48,4 +53,9 @@ const mapStateToProps = (state) => {
 };
 
 
-export default connect(mapStateToProps, { setDate, getQuestionsData, getQuestionsStudentsData, getStudentsData, getStudentsStackData })(SelectDateContainer)
+export default connect(mapStateToProps, { 
+  setDate, 
+  getQuestionsData, 
+  getQuestionsStudentsData, 
+  getStudentsData, 
+  getStudentsStackData })(SelectDateContainer)
